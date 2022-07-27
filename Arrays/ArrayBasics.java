54635ecd968d217ec4b1f7b7674fa8614fdaf33f
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2022-07-27 13:35:44 +0530

    ArrayBasics.java

---
+++ Arrays/ArrayBasics.java
@@ -0,0 +1,20 @@
+public class ArrayBasics {
+    public static void main(String[] args){
+        String[] fruits = {"apple", "banana", "orange", "grapes", "pear"};
+
+        System.out.println(fruits[0]); //Output => apple
+        System.out.println(fruits[1]); //Output => banana
+        System.out.println(fruits[2]); //Output => orange
+        System.out.println(fruits[3]); //Output => grapes
+        System.out.println(fruits[4]); //Output => pear
+
+        int primeNumbers[] = {2, 3, 5, 7, 11};
+
+        System.out.println(primeNumbers[0]); //Output => 2 
+        System.out.println(primeNumbers[1]); //Output => 3
+        System.out.println(primeNumbers[2]); //Output => 5
+        System.out.println(primeNumbers[3]); //Output => 7
+        System.out.println(primeNumbers[4]); //Output => 11
+
+    }
+}
